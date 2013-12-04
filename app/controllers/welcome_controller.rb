c74266c5ff372532410a32e99eb4225537f31346
treby <treby@atelier-nodoka.net>
2013-12-05 01:32:32 +0900

    Installed twitter-bootstrap.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @entry_list = Entry.order('id desc')
+    @entry_list = Entry.page(params[:page]).per(30).order('id desc')
   end
 end
