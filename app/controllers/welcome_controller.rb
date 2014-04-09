52679183941c54aa0e13a533600789ad3b3f57d3
treby <treby@atelier-nodoka.net>
2014-04-10 00:23:52 +0900

    Added sidebar for feeds.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @feed_list = Feed.order('updated_at desc')
     @entry_list = Entry.page(params[:page]).per(30).order('id desc')
   end
 end
