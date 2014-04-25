a5d9aa09131ee3eaba74edd6f6fe0e81a23e84ec
treby <treby@atelier-nodoka.net>
2014-04-26 01:11:55 +0900

    Updated entry_picker.

--- lib/cron/entry_picker.rb
+++ lib/cron/entry_picker.rb
@@ -4,18 +4,23 @@ class Cron::EntryPicker
   def self.init
     feed_list = Feed.all
 
+    entries = []
     feed_list.each do |feed|
-      next if feed.url.present?
+      next unless feed.url.nil?
       obj_feed = Feedzirra::Feed.fetch_and_parse(feed.feed_url)
 
-      feed.title          = obj_feed.title
       feed.url            = obj_feed.url
       feed.feed_url       = obj_feed.feed_url
+
+      feed.title          = obj_feed.title
       feed.etag           = obj_feed.etag
       feed.last_modified  = obj_feed.last_modified
 
-      save_entries(feed.id, obj_feed.entries) if feed.save
+      feed_entries = feed_entries(feed.id, obj_feed.entries)
+      entries.concat(feed_entries) if feed.save
     end
+
+    save_entries(entries)
   end
 
   def self.update
@@ -23,52 +28,69 @@ class Cron::EntryPicker
     puts Time.now
     feed_list = Feed.all
 
+    new_entries = []
     feed_list.each do |feed|
-      check_new_entries(feed)
+      obj_entry_list = new_entries!(feed)
+      feed_entry_list = feed_entries(feed.id, obj_entry_list)
+      new_entries.concat(feed_entry_list) if obj_entry_list.present?
     end
+
+    save_entries(new_entries)
   end
 
 
   private
-  def self.save_entries(feed_id, obj_entry_list)
-    obj_entry_list.reverse.each do |obj_entry|
-      puts obj_entry.title
-      next if /^PR:/ === obj_entry.title
-
+  def self.feed_entries(feed_id, obj_entry_list)
+    entry_list = []
+    obj_entry_list.each do |obj_entry|
       entry = Entry.new
-      entry.feed_id   = feed_id
       entry.title     = obj_entry.title
       entry.url       = obj_entry.url
       entry.author    = obj_entry.author
       entry.summary   = obj_entry.summary
       entry.published = obj_entry.published
+      entry.feed_id   = feed_id
+
+      entry_list.push(entry)
+    end
+
+    entry_list
+  end
+
+  def self.save_entries(entry_list)
+    entry_list.sort_by! { |entry| entry.published }
+    entry_list.each do |entry|
+      puts entry.title
+      next if /^PR:/ === entry.title
 
       entry.save
     end
   end
 
-  def self.check_new_entries(stored_feed)
+  def self.new_entries!(stored_feed)
     feed = Feedzirra::Feed.fetch_and_parse(stored_feed.feed_url)
     unless feed.instance_variable_defined?(:@last_modified)
       puts sprintf('feed is %s (%s)', feed.class, stored_feed.feed_url)
       return
     end
 
-    if stored_feed.last_modified == feed.last_modified
-      return
-    end
+    return if stored_feed.last_modified.blank?
+    return if stored_feed.last_modified == feed.last_modified
 
-    latest_entry = stored_feed.entries.last
     new_entries = []
+    latest_entry = stored_feed.entries.last
     feed.entries.each do |entry|
+      entry.feed_id = stored_feed.id
       new_entries.push(entry) if entry.published > latest_entry.published
     end
     puts sprintf('%d entries found. - %s', new_entries.count, stored_feed.title) if new_entries.present?
-    save_entries(stored_feed.id, new_entries)
 
+    # Feedの更新
     stored_feed.title = feed.title
     stored_feed.etag = feed.etag
     stored_feed.last_modified = feed.last_modified
     stored_feed.save
+
+    new_entries
   end
 end
